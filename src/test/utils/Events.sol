6cf9deb4443a3f82d44b270ac632a27b6b8d96d2
Thomas Smets <thomass-smets@hotmail.com>
2023-09-11 13:15:07 +0200

    ♻️✅ Refactor MainRegistry tests

--- src/test/utils/Events.sol
+++ src/test/utils/Events.sol
@@ -39,6 +39,15 @@ abstract contract Events {
     event OracleDecommissioned(address indexed oracle, bool isActive);
 
     /*//////////////////////////////////////////////////////////////////////////
+                                  MAIN REGISTRY
+    //////////////////////////////////////////////////////////////////////////*/
+
+    event AllowedActionSet(address indexed action, bool allowed);
+    event AssetAdded(address indexed assetAddress, address indexed pricingModule, uint8 assetType);
+    event BaseCurrencyAdded(address indexed assetAddress, uint8 indexed baseCurrencyId, bytes8 label);
+    event PricingModuleAdded(address pricingModule);
+
+    /*//////////////////////////////////////////////////////////////////////////
                                 PRICING MODULE
     //////////////////////////////////////////////////////////////////////////*/
 
