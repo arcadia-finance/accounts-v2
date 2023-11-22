7bc8432c61fdecc6cc43e57cecf9b21214b768dc
Simon L <simon.luyten@gmail.com>
2023-11-22 16:19:13 +0100

    :pencil: fix typoes and whitelines

--- src/asset-modules/StandardERC20AssetModule.sol
+++ src/asset-modules/StandardERC20AssetModule.sol
@@ -9,16 +9,18 @@ import { IRegistry } from "./interfaces/IRegistry.sol";
 import { PrimaryAssetModule } from "./AbstractPrimaryAssetModule.sol";
 
 /**
- * @title Asset Module for Standard ERC20 tokens.
+ * @title Asset Module for Standard ERC20 tokens
  * @author Pragma Labs
  * @notice The pricing logic and basic information for ERC20 tokens for which a direct price feed exists.
  * @dev No end-user should directly interact with the StandardERC20AssetModule, only the Registry
  * or the contract owner.
  */
 contract StandardERC20AssetModule is PrimaryAssetModule {
+
     /* //////////////////////////////////////////////////////////////
                                 ERRORS
     ////////////////////////////////////////////////////////////// */
+
     error Max_18_Decimals();
 
     /* //////////////////////////////////////////////////////////////
@@ -27,7 +29,7 @@ contract StandardERC20AssetModule is PrimaryAssetModule {
 
     /**
      * @param registry_ The contract address of the Registry.
-     * @dev The ASSET_TYPE, necessary for the deposit and withdraw logic in the Accounts, for ERC20 tokens is 0.
+     * @dev The ASSET_TYPE, necessary for the deposit and withdraw logic in the Accounts, for ERC20 tokens it is 0.
      */
     constructor(address registry_) PrimaryAssetModule(registry_, 0) { }
 
@@ -53,7 +55,7 @@ contract StandardERC20AssetModule is PrimaryAssetModule {
         uint256 assetUnit = 10 ** IERC20(asset).decimals();
         if (assetUnit > 1e18) revert Max_18_Decimals();
 
-        // Can safely cast to uint64, we previously checked it is smaller than 10e18.
+        // Can safely cast to uint64, we previously checked it is smaller than 1e18.
         assetToInformation[_getKeyFromAsset(asset, 0)] =
             AssetInformation({ assetUnit: uint64(assetUnit), oracleSequence: oracleSequence });
     }
@@ -63,11 +65,11 @@ contract StandardERC20AssetModule is PrimaryAssetModule {
     ///////////////////////////////////////////////////////////////*/
 
     /**
-     * @notice Checks for a token address and the corresponding Id if it is allowed.
+     * @notice Checks for a token address and the corresponding id if it is allowed.
      * @param asset The contract address of the asset.
-     * param assetId The Id of the asset.
+     * param assetId The id of the asset.
      * @return A boolean, indicating if the asset is allowed.
-     * @dev Since ERC20s don't have an Id, the Id should be set to 0.
+     * @dev Since ERC20s don't have an id, the id should be set to 0.
      */
     function isAllowed(address asset, uint256) public view override returns (bool) {
         return inAssetModule[asset];
@@ -76,9 +78,9 @@ contract StandardERC20AssetModule is PrimaryAssetModule {
     /**
      * @notice Returns the unique identifier of an asset based on the contract address and id.
      * @param asset The contract address of the asset.
-     * param assetId The Id of the asset.
+     * param assetId The id of the asset.
      * @return key The unique identifier.
-     * @dev The assetId is hard-coded to 0, since the assets for this Asset Modules are ERC20's.
+     * @dev The assetId is hard-coded to 0, since the assets for this Asset Modules are ERC20s.
      */
     function _getKeyFromAsset(address asset, uint256) internal pure override returns (bytes32 key) {
         assembly {
@@ -90,8 +92,8 @@ contract StandardERC20AssetModule is PrimaryAssetModule {
      * @notice Returns the contract address and id of an asset based on the unique identifier.
      * @param key The unique identifier.
      * @return asset The contract address of the asset.
-     * @return assetId The Id of the asset.
-     * @dev The assetId is hard-coded to 0, since the assets for this Asset Modules are ERC20's.
+     * @return assetId The id of the asset.
+     * @dev The assetId is hard-coded to 0, since the assets for this Asset Modules are ERC20s.
      */
     function _getAssetFromKey(bytes32 key) internal pure override returns (address asset, uint256) {
         assembly {
