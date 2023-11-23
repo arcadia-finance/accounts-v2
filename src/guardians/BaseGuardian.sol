ede17de2cf87e85c4073348792c8dc3b83af3337
zekiblue <zeki@arcadia.finance>
2023-11-23 19:14:59 +0100

    :recycle: Change the guardians

--- src/guardians/BaseGuardian.sol
+++ src/guardians/BaseGuardian.sol
@@ -49,6 +49,32 @@ abstract contract BaseGuardian is Owned {
         _;
     }
 
+    /**
+     * @dev Only 32 days after the last pause, the guardian can pause the protocol again.
+     * The Guardian can only pause the protocol again after 32 days have passed since the last pause.
+     * This is to prevent that a malicious guardian can take user funds hostage for an indefinite time.
+     * After the guardian has paused the protocol, the owner has 30 days to find potential problems,
+     * find a solution and unpause the protocol. If the protocol is not unpaused after 30 days,
+     * an emergency procedure can be started by any user to unpause the protocol.
+     * All users have now at least a two-day window to withdraw assets and close positions before
+     * the protocol can again be paused 32 days after the contract was previously paused.
+     */
+    modifier only32daysAfterPause() {
+        if (block.timestamp <= pauseTimestamp + 32 days) revert CannotPause();
+        _;
+    }
+
+    /**
+     * @dev If the protocol is not unpaused after 30 days, any user can unpause the protocol.
+     * This ensures that no rogue owner or guardian can lock user funds for an indefinite amount of time.
+     * All users have now at least a two-day window to withdraw assets and close positions before
+     * the protocol can again be paused 32 days after the contract was previously paused.
+     */
+    modifier only30daysAfterUnpause() {
+        if (block.timestamp <= pauseTimestamp + 30 days) revert CannotUnpause();
+        _;
+    }
+
     /* //////////////////////////////////////////////////////////////
                                 CONSTRUCTOR
     ////////////////////////////////////////////////////////////// */
@@ -73,24 +99,11 @@ abstract contract BaseGuardian is Owned {
 
     /**
      * @notice This function is used to pause all the flags of the contract.
-     * @dev This function can be called by the guardian to pause all functionality in the event of an emergency.
-     * This function pauses all functionalities.
-     * The Guardian can only pause the protocol again after 32 days have passed since the last pause.
-     * This is to prevent that a malicious guardian can take user funds hostage for an indefinite time.
-     * @dev After the guardian has paused the protocol, the owner has 30 days to find potential problems,
-     * find a solution and unpause the protocol. If the protocol is not unpaused after 30 days,
-     * an emergency procedure can be started by any user to unpause the protocol.
-     * All users have now at least a two-day window to withdraw assets and close positions before
-     * the protocol can again be paused 32 days after the contract was previously paused.
      */
-    function pause() external virtual onlyGuardian { }
+    function pause() external virtual onlyGuardian only32daysAfterPause { }
 
     /**
      * @notice This function is used to unpause all flags.
-     * @dev If the protocol is not unpaused after 30 days, any user can unpause the protocol.
-     * This ensures that no rogue owner or guardian can lock user funds for an indefinite amount of time.
-     * All users have now at least a two-day window to withdraw assets and close positions before
-     * the protocol can again be paused 32 days after the contract was previously paused.
      */
-    function unpause() external virtual;
+    function unpause() external virtual only30daysAfterUnpause { }
 }
