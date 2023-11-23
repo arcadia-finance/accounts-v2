cd4193b24a4fe0ea20e7291ee06955a29a031ed2
Alexandre Collette <alexcollette@hotmail.com>
2023-11-23 19:42:16 +0100

    :art: small fix

--- test/fuzz/accounts/AccountV1/EndAuction.fuzz.t.sol
+++ test/fuzz/accounts/AccountV1/EndAuction.fuzz.t.sol
@@ -5,13 +5,12 @@
 pragma solidity 0.8.19;
 
 import { AccountV1_Fuzz_Test } from "./_AccountV1.fuzz.t.sol";
-import { Constants } from "../../../utils/Constants.sol";
 import { AccountErrors } from "../../../../src/libraries/Errors.sol";
 
 /**
- * @notice Fuzz tests for the function "getLiquidationValue" of contract "AccountV1".
+ * @notice Fuzz tests for the function "endAuction" of contract "AccountV1".
  */
-contract GetLiquidationValue_AccountV1_Fuzz_Test is AccountV1_Fuzz_Test {
+contract endAuction_AccountV1_Fuzz_Test is AccountV1_Fuzz_Test {
     /* ///////////////////////////////////////////////////////////////
                             TEST CONTRACTS
     /////////////////////////////////////////////////////////////// */
