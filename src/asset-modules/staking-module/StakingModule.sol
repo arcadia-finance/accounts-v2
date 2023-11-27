e5d0094d84a6deb2ad3e4a26081c6e661ab1ed97
Alexandre Collette <alexcollette@hotmail.com>
2023-11-27 20:30:41 +0100

    :sparkles: :construction: init staking-module for asset modules

---
+++ src/asset-modules/staking-module/StakingModule.sol
@@ -0,0 +1,118 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.22;
+
+import { ERC20, SafeTransferLib } from "../../../lib/solmate/src/utils/SafeTransferLib.sol";
+import { FixedPointMathLib } from "../../../lib/solmate/src/utils/FixedPointMathLib.sol";
+
+contract StakingModule {
+    using FixedPointMathLib for uint256;
+    using SafeTransferLib for ERC20;
+
+    /* //////////////////////////////////////////////////////////////
+                                STORAGE
+    ////////////////////////////////////////////////////////////// */
+
+    ERC20 public immutable stakingToken;
+    ERC20 public immutable rewardsToken;
+
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) private _balances;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+
+    /* //////////////////////////////////////////////////////////////
+                                EVENTS
+    ////////////////////////////////////////////////////////////// */
+
+    event Staked(address indexed account, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    /* //////////////////////////////////////////////////////////////
+                                ERRORS
+    ////////////////////////////////////////////////////////////// */
+
+    error AmountIsZero();
+
+    /* //////////////////////////////////////////////////////////////
+                                MODIFIERS
+    ////////////////////////////////////////////////////////////// */
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    /* //////////////////////////////////////////////////////////////
+                                CONSTRUCTOR
+    ////////////////////////////////////////////////////////////// */
+
+    constructor(ERC20 stakingToken_, ERC20 rewardsToken_) {
+        stakingToken = stakingToken_;
+        rewardsToken = rewardsToken_;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        STAKING TOKEN INFORMATION
+    ///////////////////////////////////////////////////////////////*/
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    // Note: add nonReentrant and notPaused? modifiers
+    function stake(uint256 amount, address account) external updateReward(account) {
+        if (amount == 0) revert AmountIsZero();
+
+        _totalSupply += amount;
+        _balances[msg.sender] += amount;
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Internal function to stake in protocol
+
+        emit Staked(msg.sender, amount);
+    }
+
+    // Note: add nonReentrant modifier
+    function withdraw(uint256 amount, address account) public updateReward(account) {
+        if (amount == 0) revert AmountIsZero();
+
+        _totalSupply -= amount;
+        _balances[msg.sender] -= amount;
+
+        // Internal function to claim from protocol
+
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function rewardPerToken() public view returns (uint256 rewardPerToken_) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+
+        // Calc total earned amount of this contract as of now minus last time = earned over period.
+        uint256 earnedSinceLastUpdate;
+        rewardPerToken_ = rewardPerTokenStored + earnedSinceLastUpdate.mulDivDown(1e18, _totalSupply);
+    }
+
+    // Note: see if we can optimize rewardPerToken here, as we calculate it in modifier previously.
+    function earned(address account) public view returns (uint256 earned_) {
+        uint256 rewardPerTokenClaimable = rewardPerToken() - userRewardPerTokenPaid[account];
+        earned_ = rewards[account] + _balances[account].mulDivDown(rewardPerTokenClaimable, 1e18);
+    }
+}
