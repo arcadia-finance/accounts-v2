27eba84334ab45a4f6af63852755e4e287f3c93c
Alexandre Collette <alexcollette@hotmail.com>
2024-01-30 16:00:40 +0100

    :white_check_mark: tests complete except getUnderlyingAssetAmounts() for aeroVolatileAM

--- test/fuzz/asset-modules/AerodromeVolatileAM/GetUnderlyingAssets.fuzz.t.sol
+++ test/fuzz/asset-modules/AerodromeVolatileAM/GetUnderlyingAssets.fuzz.t.sol
@@ -3,3 +3,49 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 pragma solidity 0.8.22;
+
+import { AerodromeVolatileAM_Fuzz_Test } from "./_AerodromeVolatileAM.fuzz.t.sol";
+
+/**
+ * @notice Fuzz tests for the function "_getUnderlyingAssets" of contract "AerodromeVolatileAM".
+ */
+contract GetUnderlyingAssets_AerodromeVolatileAM_Fuzz_Test is AerodromeVolatileAM_Fuzz_Test {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public virtual override {
+        AerodromeVolatileAM_Fuzz_Test.setUp();
+    }
+
+    /* ///////////////////////////////////////////////////////////////
+                              TESTS
+    /////////////////////////////////////////////////////////////// */
+
+    function testFuzz_Success_getUnderlyingAssets_InAssetModule(uint256 poolId) public {
+        // Given : Valid initial state
+        setInitialState();
+
+        // And : Asset has been added to the AM
+        aeroVolatileAM.addAsset(address(aeroPoolMock));
+
+        bytes32 assetKey = bytes32(abi.encodePacked(uint96(0), address(aeroPoolMock)));
+        bytes32[] memory expectedUnderlyingAssetKeys = new bytes32[](2);
+        expectedUnderlyingAssetKeys[0] = bytes32(abi.encodePacked(uint96(0), address(mockERC20.token1)));
+        expectedUnderlyingAssetKeys[1] = bytes32(abi.encodePacked(uint96(0), address(mockERC20.stable1)));
+
+        bytes32[] memory actualUnderlyingAssetKeys = aeroVolatileAM.getUnderlyingAssets(assetKey);
+
+        assertEq(actualUnderlyingAssetKeys[0], expectedUnderlyingAssetKeys[0]);
+        assertEq(actualUnderlyingAssetKeys[1], expectedUnderlyingAssetKeys[1]);
+    }
+
+    function testFuzz_Success_getUnderlyingAssets_NotInAssetModule() public {
+        bytes32 assetKey = bytes32(abi.encodePacked(uint96(0), address(aeroPoolMock)));
+
+        bytes32[] memory underlyingAssetKeys = aeroVolatileAM.getUnderlyingAssets(assetKey);
+
+        // And: No actualUnderlyingAssetKeys are returned.
+        assertEq(underlyingAssetKeys.length, 0);
+    }
+}
--- test/fuzz/asset-modules/AerodromeVolatileAM/isAllowed.fuzz.t.sol
+++ test/fuzz/asset-modules/AerodromeVolatileAM/isAllowed.fuzz.t.sol
@@ -3,3 +3,44 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 pragma solidity 0.8.22;
+
+import { AerodromeVolatileAM_Fuzz_Test } from "./_AerodromeVolatileAM.fuzz.t.sol";
+import { stdStorage, StdStorage } from "../../../../lib/forge-std/src/StdStorage.sol";
+
+/**
+ * @notice Fuzz tests for the function "isAllowed" of contract "AerodromeVolatileAM".
+ */
+contract IsAllowed_AerodromeVolatileAM_Fuzz_Test is AerodromeVolatileAM_Fuzz_Test {
+    using stdStorage for StdStorage;
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public virtual override {
+        AerodromeVolatileAM_Fuzz_Test.setUp();
+    }
+
+    /* ///////////////////////////////////////////////////////////////
+                              TESTS
+    /////////////////////////////////////////////////////////////// */
+
+    function testFuzz_Success_isAllowed_False(address asset, uint256 id) public {
+        // When : Calling isAllowed()
+        bool allowed = aeroVolatileAM.isAllowed(asset, id);
+
+        // Then : It should return false
+        assertFalse(allowed);
+    }
+
+    function testFuzz_Success_isAllowed_True(address asset, uint256 id) public {
+        // Given: asset is in the aeroVolatileAM.
+        stdstore.target(address(aeroVolatileAM)).sig(aeroVolatileAM.inAssetModule.selector).with_key(asset)
+            .checked_write(true);
+
+        // When : Calling isAllowed()
+        bool allowed = aeroVolatileAM.isAllowed(asset, id);
+
+        // Then : It should return true
+        assertTrue(allowed);
+    }
+}
