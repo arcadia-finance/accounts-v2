c2a074458b457d8e15362598bcc2d9a8c2f1daf0
j-vp <jasper@arcadia.finance>
2024-03-18 19:06:42 +0100

    🚀 deploy step 1

--- script/Deploy_accounts_base_step_1.sol
+++ script/Deploy_accounts_base_step_1.sol
@@ -23,7 +23,7 @@ import { ActionMultiCall } from "../src/actions/MultiCall.sol";
 import { ILendingPool } from "./interfaces/ILendingPool.sol";
 import { ERC20 } from "../lib/solmate/src/tokens/ERC20.sol";
 
-contract ArcadiaAccountDeployment is Test {
+contract ArcadiaAccountDeploymentStep1 is Test {
     Factory internal factory;
     AccountV1 internal account;
 
@@ -92,13 +92,14 @@ contract ArcadiaAccountDeployment is Test {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOYER_BASE");
         address deployerAddress = vm.addr(deployerPrivateKey);
+        address protocolOwnerAddress = DeployAddresses.protocolOwner_base;
 
         vm.startBroadcast(deployerPrivateKey);
         factory = Factory(0x38dB790e1894A5863387B43290c8340121e7Cd48); //todo: change after factory deploy
         wethLendingPool = ILendingPool(0xA04B08324745AEc82De30c3581c407BE63E764c8); //todo: change after LP deploy
         usdcLendingPool = ILendingPool(0xEda73DA39Aae3282DDC2Dc924c740574567FFabc); //todo: change after LP deploy
-        wethLendingPool.setRiskManager(deployerAddress);
-        usdcLendingPool.setRiskManager(deployerAddress);
+        wethLendingPool.setRiskManager(protocolOwnerAddress);
+        usdcLendingPool.setRiskManager(protocolOwnerAddress);
 
         registry = new Registry(address(factory), DeployAddresses.sequencerUptimeOracle_base);
 
@@ -157,165 +158,55 @@ contract ArcadiaAccountDeployment is Test {
         stakedStargateAM.initialize();
 
         factory.setNewAccountInfo(address(registry), address(account), DeployBytes.upgradeRoot1To1, "");
-        factory.changeGuardian(deployerAddress);
+        factory.changeGuardian(protocolOwnerAddress);
 
-        registry.changeGuardian(deployerAddress);
+        registry.changeGuardian(protocolOwnerAddress);
 
         wethLendingPool.setAccountVersion(1, true);
         usdcLendingPool.setAccountVersion(1, true);
 
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.comp_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.comp_collFact_1,
-            DeployRiskConstantsBase.comp_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.dai_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.dai_collFact_1,
-            DeployRiskConstantsBase.dai_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.weth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.eth_collFact_1,
-            DeployRiskConstantsBase.eth_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.usdc_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.usdc_collFact_1,
-            DeployRiskConstantsBase.usdc_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.usdbc_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.usdbc_collFact_1,
-            DeployRiskConstantsBase.usdbc_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.cbeth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.cbeth_collFact_1,
-            DeployRiskConstantsBase.cbeth_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.reth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.reth_collFact_1,
-            DeployRiskConstantsBase.reth_liqFact_1
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(wethLendingPool),
-            DeployAddresses.stg_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.stg_collFact_1,
-            DeployRiskConstantsBase.stg_liqFact_1
-        );
-
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.comp_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.comp_collFact_2,
-            DeployRiskConstantsBase.comp_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.dai_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.dai_collFact_2,
-            DeployRiskConstantsBase.dai_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.weth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.eth_collFact_2,
-            DeployRiskConstantsBase.eth_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.usdc_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.usdc_collFact_2,
-            DeployRiskConstantsBase.usdc_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.usdbc_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.usdbc_collFact_2,
-            DeployRiskConstantsBase.usdbc_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.cbeth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.cbeth_collFact_2,
-            DeployRiskConstantsBase.cbeth_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.reth_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.reth_collFact_2,
-            DeployRiskConstantsBase.reth_liqFact_2
-        );
-        registry.setRiskParametersOfPrimaryAsset(
-            address(usdcLendingPool),
-            DeployAddresses.stg_base,
-            0,
-            type(uint112).max,
-            DeployRiskConstantsBase.stg_collFact_2,
-            DeployRiskConstantsBase.stg_liqFact_2
-        );
-
-        registry.setRiskParametersOfDerivedAM(
-            address(usdcLendingPool), address(uniswapV3AM), 15 * 10 ** 6 * 10 ** 18, 9800
-        );
-        registry.setRiskParametersOfDerivedAM(
-            address(wethLendingPool), address(uniswapV3AM), 15 * 10 ** 6 * 10 ** 18, 9800
-        );
-        registry.setRiskParametersOfDerivedAM(
-            address(usdcLendingPool), address(stargateAM), 15 * 10 ** 6 * 10 ** 18, 9700
-        );
-        registry.setRiskParametersOfDerivedAM(
-            address(wethLendingPool), address(stargateAM), 15 * 10 ** 6 * 10 ** 18, 9700
-        );
-        registry.setRiskParametersOfDerivedAM(
-            address(usdcLendingPool), address(stakedStargateAM), 15 * 10 ** 6 * 10 ** 18, 9800
-        );
-        registry.setRiskParametersOfDerivedAM(
-            address(wethLendingPool), address(stakedStargateAM), 15 * 10 ** 6 * 10 ** 18, 9800
-        );
-
-        registry.setRiskParameters(address(usdcLendingPool), 0, 15 minutes, 5);
-        registry.setRiskParameters(address(wethLendingPool), 0, 15 minutes, 5);
-
         vm.stopBroadcast();
+    }
+
+    function test_deploy() public {
+        address protocolOwnerAddress = DeployAddresses.protocolOwner_base;
+
+        assertEq(factory.owner(), protocolOwnerAddress);
+        assertEq(factory.guardian(), protocolOwnerAddress);
+
+        assertEq(registry.owner(), protocolOwnerAddress);
+        assertEq(registry.guardian(), protocolOwnerAddress);
+
+        assertEq(account.owner(), protocolOwnerAddress);
+        assertEq(account.registry(), address(registry));
+        assertEq(account.liquidator(), address(0));
+        assertEq(account.minimumMargin(), 0);
+        assertEq(account.numeraire(), address(0));
+        assertEq(account.creditor(), address(0));
+
+        assertEq(wethLendingPool.isValidVersion(account.ACCOUNT_VERSION()), true);
+        assertEq(usdcLendingPool.isValidVersion(account.ACCOUNT_VERSION()), true);
+        assertEq(wethLendingPool.riskManager(), protocolOwnerAddress);
+        assertEq(usdcLendingPool.riskManager(), protocolOwnerAddress);
+
+        assertTrue(registry.inRegistry(address(comp)));
+        assertTrue(registry.inRegistry(address(dai)));
+        assertTrue(registry.inRegistry(address(weth)));
+        assertTrue(registry.inRegistry(address(usdc)));
+        assertTrue(registry.inRegistry(address(usdbc)));
+        assertTrue(registry.inRegistry(address(cbeth)));
+        assertTrue(registry.inRegistry(address(reth)));
+        assertTrue(registry.inRegistry(address(stg)));
+
+        assertTrue(erc20PrimaryAM.inAssetModule(address(comp)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(dai)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(weth)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(usdc)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(usdbc)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(cbeth)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(reth)));
+        assertTrue(erc20PrimaryAM.inAssetModule(address(stg)));
+
+        assertTrue(uniswapV3AM.inAssetModule(DeployAddresses.uniswapV3PositionMgr_base));
     }
 }
