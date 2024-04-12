a37ba3d036028d45cb881ec89d94762f48bacaa6
Thomas Smets <thomass-smets@hotmail.com>
2024-04-12 13:17:49 +0200

    🎨 forme fmt

--- test/CompileDifferentPragmaVersions.t.sol
+++ test/CompileDifferentPragmaVersions.t.sol
@@ -7,7 +7,8 @@ pragma experimental ABIEncoderV2;
 
 import { Test } from "../lib/forge-std/src/Test.sol";
 
-import { NonfungiblePositionManagerExtension } from "./utils/fixtures/uniswap-v3/extensions/NonfungiblePositionManagerExtension.sol";
+import { NonfungiblePositionManagerExtension } from
+    "./utils/fixtures/uniswap-v3/extensions/NonfungiblePositionManagerExtension.sol";
 import { UniswapV3FactoryExtension } from "./utils/fixtures/uniswap-v3/extensions/UniswapV3FactoryExtension.sol";
 import { UniswapV3PoolExtension } from "./utils/fixtures/uniswap-v3/extensions/UniswapV3PoolExtension.sol";
 
