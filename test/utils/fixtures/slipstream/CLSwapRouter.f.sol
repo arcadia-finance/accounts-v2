936393cb0c1c0d916f53ba798d566177b2e35952
Thomas Smets <thomass-smets@hotmail.com>
2024-08-30 11:28:36 +0200

    ✨ add slipstream fixtures

---
+++ test/utils/fixtures/slipstream/CLSwapRouter.f.sol
@@ -0,0 +1,31 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.22;
+
+import { Test } from "../../../../lib/forge-std/src/Test.sol";
+
+import { ICLSwapRouter } from "./interfaces/ICLSwapRouter.sol";
+import { Utils } from "../../../utils/Utils.sol";
+
+contract CLSwapRouterFixture is Test {
+    /*//////////////////////////////////////////////////////////////////////////
+                                   CONTRACTS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    ICLSwapRouter internal clSwapRouter;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                  SET-UP FUNCTION
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function deploySwapRouter(address factory_, address weth9_) public {
+        // Get the bytecode of the SwapRouterExtension.
+        bytes memory args = abi.encode(factory_, weth9_);
+        bytes memory bytecode = abi.encodePacked(vm.getCode("SwapRouter.sol"), args);
+
+        address swapRouter_ = Utils.deployBytecode(bytecode);
+        clSwapRouter = ICLSwapRouter(swapRouter_);
+    }
+}
---
+++ test/utils/fixtures/uniswap-v3/QuoterV2Fixture.f.sol
@@ -0,0 +1,41 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.22;
+
+import { Test } from "../../../../lib/forge-std/src/Test.sol";
+
+import { IQuoterV2 } from "./extensions/interfaces/IQuoterV2.sol";
+import { Utils } from "../../../utils/Utils.sol";
+
+contract QuoterV2Fixture is Test {
+    /*//////////////////////////////////////////////////////////////////////////
+                                   CONTRACTS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    IQuoterV2 internal quoter;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                  SET-UP FUNCTION
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function deployQuoterV2(address factoryV3_, address weth9_) public {
+        // Get the bytecode of the UniswapV3PoolExtension.
+        bytes memory args = abi.encode();
+        bytes memory bytecode = abi.encodePacked(vm.getCode("UniswapV3PoolExtension.sol"), args);
+        bytes32 poolExtensionInitCodeHash = keccak256(bytecode);
+
+        // Get the bytecode of the QuoterV2.
+        args = abi.encode(factoryV3_, weth9_);
+        bytecode = abi.encodePacked(vm.getCode("QuoterV2Extension.sol"), args);
+
+        // Overwrite constant in bytecode of QuoterV2.
+        // -> Replace the code hash of UniswapV3Pool.sol with the code hash of UniswapV3PoolExtension.sol
+        bytes32 POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+        bytecode = Utils.veryBadBytesReplacer(bytecode, POOL_INIT_CODE_HASH, poolExtensionInitCodeHash);
+
+        address quoter_ = Utils.deployBytecode(bytecode);
+        quoter = IQuoterV2(quoter_);
+    }
+}
