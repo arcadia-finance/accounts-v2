2825d7493aa8ed561cdee410ebb0247e109da274
Thomas Smets <thomass-smets@hotmail.com>
2024-09-12 16:42:37 +0200

    ♻️ refactor fixtures

--- test/utils/fixtures/uniswap-v3/extensions/interfaces/IQuoterV2.sol
+++ test/utils/fixtures/uniswap-v3/extensions/interfaces/IQuoterV2.sol
@@ -5,6 +5,30 @@
 pragma solidity 0.8.22;
 
 interface IQuoterV2 {
+    struct QuoteExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint256 amountOut;
+        uint24 fee;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    struct QuoteExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint256 amountIn;
+        uint24 fee;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    function quoteExactInputSingle(QuoteExactInputSingleParams memory params)
+        external
+        returns (uint256 amountOut, uint160 sqrtPriceX96After, uint32 initializedTicksCrossed, uint256 gasEstimate);
+
+    function quoteExactOutputSingle(QuoteExactOutputSingleParams memory params)
+        external
+        returns (uint256 amountIn, uint160 sqrtPriceX96After, uint32 initializedTicksCrossed, uint256 gasEstimate);
+
     function quoteExactInput(bytes memory path, uint256 amountIn)
         external
         returns (
