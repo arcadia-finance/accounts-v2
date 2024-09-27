380e99c964c4e9c0780d36bd36174357324a63e8
Alex Collette <alexcollette@hotmail.com>
2024-09-27 11:32:12 +0200

    :white_check_mark: add test for unallowed hook

--- test/fuzz/asset-modules/UniswapV4AM/AddAsset.fuzz.t.sol
+++ test/fuzz/asset-modules/UniswapV4AM/AddAsset.fuzz.t.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.22;
 import { UniswapV4AM_Fuzz_Test } from "./_UniswapV4AM.fuzz.t.sol";
 
 import { AssetModule } from "../../../../src/asset-modules/abstracts/AbstractAM.sol";
+import { TickMath } from "../../../../lib/v4-periphery-fork/lib/v4-core/src/libraries/TickMath.sol";
 import { UniswapV4AM } from "../../../../src/asset-modules/UniswapV4/UniswapV4AM.sol";
 
 /**
@@ -45,6 +46,40 @@ contract AddAsset_UniswapV4AM_Fuzz_Test is UniswapV4AM_Fuzz_Test {
         // Then : It should revert
         vm.startPrank(users.owner);
         vm.expectRevert(UniswapV4AM.ZeroLiquidity.selector);
+        uniswapV4AM.addAsset(tokenId);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_addAsset_HooksNotAllowed(
+        uint80 tokenId,
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 liquidity
+    ) public {
+        // Given : Valid ticks
+        (tickLower, tickUpper) = givenValidTicks(tickLower, tickUpper);
+
+        // Initializes a pool with hooks that are not allowed
+        stablePoolKey = initializePool(
+            address(mockERC20.stable1),
+            address(mockERC20.stable2),
+            TickMath.getSqrtPriceAtTick(0),
+            address(unvalidHook),
+            500,
+            10
+        );
+
+        // And : Liquidity is not-zero
+        vm.assume(liquidity > 0);
+        bytes32 positionKey =
+            keccak256(abi.encodePacked(address(positionManager), tickLower, tickUpper, bytes32(uint256(tokenId))));
+        poolManager.setPositionLiquidity(stablePoolKey.toId(), positionKey, liquidity);
+        positionManager.setPosition(users.owner, stablePoolKey, tickLower, tickUpper, tokenId);
+
+        // When : Calling addAsset() for a pool that has unallowed hooks
+        // Then : It should revert
+        vm.startPrank(users.owner);
+        vm.expectRevert(UniswapV4AM.HooksNotAllowed.selector);
         uniswapV4AM.addAsset(tokenId);
         vm.stopPrank();
     }
