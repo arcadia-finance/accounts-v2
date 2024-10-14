47596e9e174f8532e7c4de08b205ac7268a9a1a2
Thomas Smets <thomass-smets@hotmail.com>
2024-10-14 14:21:31 +0200

    🔥 remove unnecessary hooks from library

--- src/asset-modules/UniswapV4/libraries/Hooks.sol
+++ src/asset-modules/UniswapV4/libraries/Hooks.sol
@@ -2,27 +2,8 @@
 pragma solidity ^0.8.22;
 
 library Hooks {
-    uint160 internal constant ALL_HOOK_MASK = uint160((1 << 14) - 1);
-
-    uint160 internal constant BEFORE_INITIALIZE_FLAG = 1 << 13;
-    uint160 internal constant AFTER_INITIALIZE_FLAG = 1 << 12;
-
-    uint160 internal constant BEFORE_ADD_LIQUIDITY_FLAG = 1 << 11;
-    uint160 internal constant AFTER_ADD_LIQUIDITY_FLAG = 1 << 10;
-
     uint160 internal constant BEFORE_REMOVE_LIQUIDITY_FLAG = 1 << 9;
     uint160 internal constant AFTER_REMOVE_LIQUIDITY_FLAG = 1 << 8;
-
-    uint160 internal constant BEFORE_SWAP_FLAG = 1 << 7;
-    uint160 internal constant AFTER_SWAP_FLAG = 1 << 6;
-
-    uint160 internal constant BEFORE_DONATE_FLAG = 1 << 5;
-    uint160 internal constant AFTER_DONATE_FLAG = 1 << 4;
-
-    uint160 internal constant BEFORE_SWAP_RETURNS_DELTA_FLAG = 1 << 3;
-    uint160 internal constant AFTER_SWAP_RETURNS_DELTA_FLAG = 1 << 2;
-    uint160 internal constant AFTER_ADD_LIQUIDITY_RETURNS_DELTA_FLAG = 1 << 1;
-    uint160 internal constant AFTER_REMOVE_LIQUIDITY_RETURNS_DELTA_FLAG = 1 << 0;
 
     function hasPermission(uint160 hook, uint160 flag) internal pure returns (bool) {
         return hook & flag != 0;
