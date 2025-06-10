a06d4c710c101810af8358328f94a475a6450c00
Thomas Smets <thomass-smets@hotmail.com>
2025-06-10 13:52:53 +0200

    ✨ add vvv

--- script/add-assets/AddAsset.s.sol
+++ script/add-assets/AddAsset.s.sol
@@ -4,13 +4,13 @@
  */
 pragma solidity ^0.8.22;
 
-import { AerodromeGauges, Assets, Oracles, Safes } from "../utils/constants/Base.sol";
+import { AerodromeGauges, AerodromePools, Assets, Oracles, Safes } from "../utils/constants/Base.sol";
 import { Asset, Oracle } from "../utils/constants/Base.sol";
 import { Base_Script } from "../Base.s.sol";
 
 contract AddAsset is Base_Script {
-    Asset internal ASSET = Assets.LBTC();
-    Oracle internal ORACLE = Oracles.LBTC_USD();
+    Asset internal ASSET = Assets.VVV();
+    Oracle internal ORACLE = Oracles.VVV_USD();
     address internal SAFE = Safes.OWNER;
 
     constructor() { }
@@ -22,8 +22,18 @@ contract AddAsset is Base_Script {
         // Add Primary asset.
         addToBatch(SAFE, address(erc20PrimaryAM), addAsset(ASSET, ORACLE));
 
+        // Add Aerodrome pools to aerodrome AM.
+        bytes memory calldata_ = abi.encodeCall(aerodromePoolAM.addAsset, (AerodromePools.V_WETH_VVV));
+        addToBatch(SAFE, address(aerodromePoolAM), calldata_);
+
+        // Add Aerodrome gauge to staked Aerodrome AM.
+        calldata_ = abi.encodeCall(stakedAerodromeAM.addAsset, (AerodromeGauges.V_WETH_VVV));
+        addToBatch(SAFE, address(stakedAerodromeAM), calldata_);
+
         // Add Aerodrome gauge to Staked Slipstream AM.
-        bytes memory calldata_ = abi.encodeCall(stakedSlipstreamAM.addGauge, (AerodromeGauges.CL1_CBBTC_LBTC));
+        calldata_ = abi.encodeCall(stakedSlipstreamAM.addGauge, (AerodromeGauges.CL100_WETH_VVV));
+        addToBatch(SAFE, address(stakedSlipstreamAM), calldata_);
+        calldata_ = abi.encodeCall(stakedSlipstreamAM.addGauge, (AerodromeGauges.CL200_WETH_VVV));
         addToBatch(SAFE, address(stakedSlipstreamAM), calldata_);
 
         // Create and write away batched transaction data to be signed with Safe.
