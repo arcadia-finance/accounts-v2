b3c3186bd444a53492d998d1cb2a52e184fcc186
Thomas Smets <thomass-smets@hotmail.com>
2025-08-27 11:42:22 +0200

    ⬆️ fix

--- test/utils/fixtures/merkl/MerklFixture.f.sol
+++ test/utils/fixtures/merkl/MerklFixture.f.sol
@@ -4,8 +4,17 @@
  */
 pragma solidity ^0.8.22;
 
+import { AccessControlManager } from "../../../../lib/merkl-contracts/contracts/AccessControlManager.sol";
+import { DistributionCreator } from "../../../../lib/merkl-contracts/contracts/DistributionCreator.sol";
 import { Distributor } from "../../../../lib/merkl-contracts/contracts/Distributor.sol";
+import { ERC1967Proxy } from
+    "../../../../lib/openzeppelin-contracts-upgradeable-v4.9/lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { IAccessControlManager } from "../../../../lib/merkl-contracts/contracts/interfaces/IAccessControlManager.sol";
+import { ProxyAdmin } from
+    "../../../../lib/openzeppelin-contracts-upgradeable-v4.9/lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 import { Test } from "../../../../lib/forge-std/src/Test.sol";
+import { TransparentUpgradeableProxy } from
+    "../../../../lib/openzeppelin-contracts-upgradeable-v4.9/lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract MerklFixture is Test {
     /*//////////////////////////////////////////////////////////////////////////
@@ -16,14 +25,43 @@ contract MerklFixture is Test {
                                    CONTRACTS
     //////////////////////////////////////////////////////////////////////////*/
 
+    AccessControlManager internal accessControlManager;
+    DistributionCreator internal distributionCreator;
     Distributor internal distributor;
+    ProxyAdmin internal proxyAdmin;
 
     /*//////////////////////////////////////////////////////////////////////////
                                   SET-UP FUNCTION
     //////////////////////////////////////////////////////////////////////////*/
 
-    function deployMerkl() internal {
-        distributor = new Distributor();
+    function deployMerkl(address owner) internal {
+        // Deploy ProxyAdmin.
+        proxyAdmin = new ProxyAdmin();
+        proxyAdmin.transferOwnership(owner);
+
+        // Deploy AccessControlManager.
+        AccessControlManager implementation = new AccessControlManager();
+        bytes memory initData = abi.encodeCall(AccessControlManager.initialize, (address(this), owner));
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(implementation), address(proxyAdmin), initData);
+        accessControlManager = AccessControlManager(address(proxy));
+        accessControlManager.addGovernor(owner);
+
+        // Deploy Distributor.
+        Distributor implementation_ = new Distributor();
+        ERC1967Proxy proxy_ = new ERC1967Proxy(address(implementation_), "");
+        distributor = Distributor(address(proxy_));
+        distributor.initialize(IAccessControlManager(address(accessControlManager)));
+
+        // Deploy DistributionCreator.
+        DistributionCreator implementation__ = new DistributionCreator();
+        proxy_ = new ERC1967Proxy(address(implementation__), "");
+        distributionCreator = DistributionCreator(address(proxy_));
+        distributionCreator.initialize(
+            IAccessControlManager(address(accessControlManager)),
+            address(distributor),
+            0.03 gwei // 0.03 gwei
+        );
     }
 
     /*//////////////////////////////////////////////////////////////////////////
