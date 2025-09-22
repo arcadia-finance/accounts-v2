f65fe007ea46623bf832bcc2612810ff156fe7c5
Thomas Smets <thomass-smets@hotmail.com>
2025-09-22 15:12:01 +0200

    ✨ add redstone push oracle module

---
+++ src/oracle-modules/RedStonePushOM.sol
@@ -0,0 +1,24 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity ^0.8.30;
+
+import { ChainlinkOM } from "./ChainlinkOM.sol";
+
+/**
+ * @title Redstone Push Oracle Module
+ * @author Pragma Labs
+ * @notice Oracle Module for Redstone Push Oracles.
+ */
+contract RedStonePushOM is ChainlinkOM {
+    /* //////////////////////////////////////////////////////////////
+                                CONSTRUCTOR
+    ////////////////////////////////////////////////////////////// */
+
+    /**
+     * @param owner_ The address of the Owner.
+     * @param registry_ The contract address of the Registry.
+     */
+    constructor(address owner_, address registry_) ChainlinkOM(owner_, registry_) { }
+}
